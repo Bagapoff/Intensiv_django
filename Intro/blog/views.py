0fcf2d86447d3db27746e3d5da67203ae2a41f3c
arif_bagapov <bagapov99@mail.ru>
2020-07-30 22:24:20 +0300

    New page 'Customers'

--- Intro/blog/views.py
+++ Intro/blog/views.py
@@ -2,7 +2,20 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello World! <a href="/contacts">Контакты</a>')
+    return HttpResponse('<h1>Главная страница</h1><br> Hello world!<br> '
+                        '<a href="/contacts">Контакты</a>  '
+                        '<a href="/customers">Покупатели</a>')
 
 def contacts(request):
-    return HttpResponse('Email:123345.ru <a href="/">Назад</a>')
+    return HttpResponse('<h1>Контакты</h1>'
+                        'Email:123345.ru<br>'
+                        'Тел. 88005553535<br>'
+                        '<a href="/">Главная</a>  '
+                        '<a href="/customers">Покупатели</a>')
+
+def customers(request):
+    return HttpResponse('<h1>Покупатели</h1>'
+                        'Ivan<br>'
+                        'Petr<br>'
+                        'Andrej<br> <a href="/">Главная</a>  '
+                        '<a href="/contacts">Контакты</a>')
